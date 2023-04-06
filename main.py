dbbb97063c38d1557cf83f59f4a2da3c32c72415
markkhomenko <2markkhomenko@gmail.com>
2023-04-06 20:50:32 +0300

    change logic that counts max dividend of 2 or fewer number of lists

--- main.py
+++ main.py
@@ -11,14 +11,16 @@ def main():
     func(data, N)
 def numbers_combinations(skip_index, select_index, list_num, possible_combinations, N):
     tmp_dict = {}
-    max_index_value = list_num[select_index] [0]
+    max_index_value = list_num[select_index][0]
     tmp_dict[select_index] = max_index_value
+    for i, val in enumerate(list_num):
+        if i != skip_index and i != select_index:
+            tmp_dict[i] = val[0]
     while True:
-        for i, val in enumerate(list_num):
-            if i != skip_index and i != select_index:
-                tmp_dict[i] = val[0]
         if sum(tmp_dict.values()) % N == 0:
-            search_min_delta(tmp_dict, list_num)
+            if search_min_delta(tmp_dict, list_num) == 0:
+                tmp_dict = {}
+                break
         else:
             break
     possible_combinations.append(tmp_dict)
@@ -31,19 +33,27 @@ def search_min_delta(tmp_dict, list_num):
             if delta == 0:
                 continue
             dict_sum_list[k] = delta
+    print(dict_sum_list)
+    if not dict_sum_list:
+        return 0
     min_delta_key = min(dict_sum_list, key=dict_sum_list.get)
-    del tmp_dict[min_delta_key]
+    tmp_dict[min_delta_key] = list_num [min_delta_key] [1]
     list_num[min_delta_key].pop(0)
+
+
 def func(data, N):
     possible_combinations = []
     list_num = [sorted(set(lst), reverse=True) for lst in data]
+    if len(list_num) < 2:
+         return next(0)
     select_index = 1
     for skip_index in range(len(list_num)):
         numbers_combinations(skip_index, select_index, list_num, possible_combinations, N)
         select_index += 1
         if select_index == len(list_num):
             select_index = 0
-    print(possible_combinations)
+    next(possible_combinations)
 
-
+def next(ps):
+    print(ps)
 main()
